7909682e077fcf307d6f06f8e37ecdc70f21e3ca
Flavialice <pirvuflaviaalice@gmail.com>
2015-06-24 03:09:09 +0300

    Client Arduino
    Sunt doua versiuni de client

---
+++ arduino/WebClient/WebClient.ino
@@ -0,0 +1,56 @@
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+
+byte mac[] = { 0x98, 0x4F, 0xEE, 0x01, 0xEA, 0x7C };
+IPAddress server(192,168,2,106);
+
+EthernetClient client;
+
+void setup() {
+  Serial.begin(9600);
+   while (!Serial) {
+    ; 
+  }
+
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    for(;;)
+      ;
+  }
+
+  delay(1000);
+  Serial.println("connecting...");
+  delay(1000);
+  
+  if (client.connect(server, 9090)) {
+    Serial.println("connected");
+
+    client.println("GET http://192.168.2.106/proj/rest/hello/ HTTP/1.1");
+    client.println();
+  } 
+  else {
+    Serial.println("connection failed");
+  }
+}
+
+void loop()
+{
+  if (client.available()) {
+    client.println(analogRead(A0));
+    client.println();
+    char c = client.read();
+    Serial.print(c);
+  }
+  
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    for(;;)
+      ;
+  }
+}
+
---
+++ arduino/WebClient1_2/WebClient1_2.ino
@@ -0,0 +1,69 @@
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+
+byte mac[] = { 0x98, 0x4F, 0xEE, 0x01, 0xEA, 0x7C };
+IPAddress server(192,168,2,106);
+
+EthernetClient client;
+
+EthernetClient clientRest;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    for(;;)
+      ;
+  }
+  
+  
+   if (clientRest.connect(server, 9090)) {
+    //Serial.println("connected");
+
+    clientRest.println("GET http://192.168.2.106/proj/rest/hello/");
+    clientRest.println();
+  } 
+  else {
+    Serial.println("connection failed");
+  }
+  
+  
+}
+
+void loop()
+{
+  //Serial.println("connecting...");
+  delay(1000);
+  
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  //Serial.println(voltage);
+  if (client.connect(server, 7070)) {
+    client.println(voltage);
+    //client.println();
+    //char c = client.read();
+    //Serial.print(c);
+  }
+  
+  if (clientRest.available()) {
+    char c = clientRest.read();
+    Serial.print(c);
+  }
+  
+  
+  
+  {
+    client.stop();
+    //Serial.println();
+    //Serial.println("disconnecting.");
+    
+
+    //for(;;)
+      //;
+  }
+  delay(1000);
+}
+
