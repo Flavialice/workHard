3dd9a2017360643d21018b857607fc82e88e0058
Flavialice <pirvuflaviaalice@gmail.com>
2015-07-13 01:38:35 +0300

    Update arduino client

--- arduino/WebClient1_2/WebClient1_2.ino
+++ arduino/WebClient1_2/WebClient1_2.ino
@@ -4,14 +4,12 @@
 int led = 7;
 byte mac[] = { 0x98, 0x4F, 0xEE, 0x01, 0xEA, 0x7C };
 IPAddress server(192,168,2,106);
-
 EthernetClient client;
-EthernetClient clientRest;
 
 void setup() {
   pinMode(led, OUTPUT); 
   Serial.begin(9600);
-
+  
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     for(;;)
@@ -22,42 +20,40 @@ void setup() {
 void loop()
 {
   //Serial.println("connecting...");
-  delay(500);
+  delay(1000);
   
-  int valoareIluminare = analogRead(A0);
-  Serial.println(valoareIluminare, DEC); 
+  int valoareIluminare = analogRead(A1);
+  //Serial.println(valoareIluminare, DEC); 
   delay(20);
   if(valoareIluminare<150)
     digitalWrite(led, HIGH);
   else
     digitalWrite(led, LOW); 
   
-  int sensorValue = analogRead(A0);
-  float voltage = sensorValue * (5.0 / 1023.0);
+  //int sensorValue = analogRead(A0);
+  //float voltage = sensorValue * (5.0 / 1023.0);
   //Serial.println(voltage);
   if (client.connect(server, 7070)) {
-    
-    
+    int valoareIluminare = analogRead(A1);
     client.print(valoareIluminare);
     //delay(10);
-    client.print(",temp");
+    client.println(",temp");
+    delay(10);
     //client.println();
-    //char c = client.read();
-    //Serial.print(c);
   }
-  
-  if (clientRest.available()) {
-    char c = clientRest.read();
+  for(int i=0;i<5;i++)
+  if (client.available())
+  {
+    char c = client.read();
     Serial.print(c);
   }
-  {
-    client.stop();
+     
     //Serial.println();
     //Serial.println("disconnecting.");
-    
     //for(;;)
       //;
-  }
+
   delay(500);
+  client.stop();
 }
 
