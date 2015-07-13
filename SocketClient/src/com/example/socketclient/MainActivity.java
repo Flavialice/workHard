8eef097255c037342884e84cba753f2bc538e644
Flavialice <pirvuflaviaalice@gmail.com>
2015-07-14 02:38:44 +0300

    Clean + URL string

--- SocketClient/src/com/example/socketclient/MainActivity.java
+++ SocketClient/src/com/example/socketclient/MainActivity.java
@@ -1,25 +1,19 @@
 package com.example.socketclient;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private String URL="192.168.2.106";
 	private Socket client;
 	private PrintWriter printwriter;
 	private EditText textField_tip;
@@ -69,12 +63,6 @@ public class MainActivity extends Activity {
 
 			try {
 
-				/*
-				 * textField_id.setText(""); textField_tip.setText("");
-				 * textField_valoare.setText(""); textField_vazut.setText("");
-				 * textField_status.setText("");
-				 */
-
 				tip = textField_tip.getText().toString();
 				valoare = textField_valoare.getText().toString();
 				status = textField_status.getText().toString();
@@ -82,22 +70,13 @@ public class MainActivity extends Activity {
 				String senzor = "tip," + tip + ",valoare," + valoare
 						+ ",status," + status;
 
-				client = new Socket("192.168.2.106", 7070);
+				client = new Socket(URL, 6060);
 				printwriter = new PrintWriter(client.getOutputStream(), true);
 				printwriter.write(senzor.toString());
 
-				/*
-				 * BufferedReader input = new BufferedReader(new
-				 * InputStreamReader(client.getInputStream())); String answer =
-				 * input.readLine();
-				 */
-				// textField.setText(answer);
 				printwriter.flush();
 				printwriter.close();
 				client.close();
-				// Toast t= Toast.makeText(getApplicationContext(),
-				// "Connection closed", 1);
-				// t.show();
 
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
